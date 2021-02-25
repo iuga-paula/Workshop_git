42594366fa1f5233554f340be0b9727f1bf4d898
Paula Iuga <paula.iuga@s.unibuc.ro>
2021-02-25 18:57:01 +0200

    adaug main pentru program.c ( asta e un mesaj)
    0-1 Knapsak problem acum e adaugata(asta e o descriere)

--- program.c
+++ program.c
@@ -32,4 +32,12 @@ int knapSack(int W, int wt[], int val[], int n)
 // Driver program to test above function
 int main()
 {
+	 int val[] = { 60, 100, 120 };
+    int wt[] = { 10, 20, 30 };
+    int W = 50;
+    int n = sizeof(val) / sizeof(val[0]);
+    printf("%d", knapSack(W, wt, val, n));
+    return 0;
+	
+	//source : GeeksforGeeks
 }
